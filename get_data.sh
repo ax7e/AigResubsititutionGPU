63b343a559f36431a5a74a3b2cfd6983d4bed865
Zhewen Hao <haozhewen@gmail.com>
2022-04-11 20:53:00 +0800

    Add mtm dataset

--- get_data.sh
+++ get_data.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 wget -c https://zenodo.org/record/2572934/files/EPFL_complete.tar.gz?download=1
+tar -xvf EPFL_complete.tar.gz
+cp ./EPFLfull/MtM/sixteen/aiger/sixteen.aig ./mockturtle/experiments/benchmarks
+cp ./EPFLfull/MtM/twenty/aiger/twenty.aig ./mockturtle/experiments/benchmarks
+cp ./EPFLfull/MtM/twentythree/aiger/twentythree.aig ./mockturtle/experiments/benchmarks
